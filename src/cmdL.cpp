0c1886de9485aa5ca4f3e41b9d2bf07785e29308
Morgan Titsworth <titsworthmorgan@gmail.com>
2025-11-25 11:00:24 -0800

    Lots added
    Got in the zone and added word Line count as well.
    Will need to test both.

---
+++ src/cmdL.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include "cmdL.hpp"
+using namespace std;
+ostringstream oss;
+
+string displayHelp()
+{
+    oss << "Command Line Tool Help Information\n"
+        << "Usage: bash test.sh [options] [filename]\n"
+        << "Options:\n"
+        << "  -h        Display this help information\n"
+        << "  -c        Count bytes in the specified file\n"
+        << "  -l        Count lines in the specified file\n"
+        << "  -w        Count words in the specified file\n";
+    cout << oss.str(); // display help
+    return oss.str();
+}
+
+int countBytes(ifstream &inFile)
+{
+    // clear any eof flags
+    inFile.clear(); 
+
+    // skip to end
+    inFile.seekg(0, ios::end);
+    
+    // return position as byte count
+    return inFile.tellg();
+}
+
+int countLines(ifstream &inFile)
+{
+    // line variable
+    string line;
+
+    // counter
+    int lineCount = 0;
+
+    // clear any eof flags
+    inFile.clear();
+
+    // seek to beginning
+    inFile.seekg(0, ios::beg);
+
+    // while line exists, increment count
+    while (getline(inFile, line))
+    {
+        lineCount++;
+    }
+
+    // return count
+    return lineCount;
+}
+
+void clearOss()
+{
+    oss.str(""); // clear the stringstream
+    oss.clear(); // clear any error flags
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 #include <sstream>
 #include <string>
 #include <iomanip>
+#include <cstring>
 using namespace std;
-stringstream oss;
 enum CommandLineOptions
 {
     h, // help
@@ -18,62 +18,90 @@ enum CommandLineOptions
 
 int main(int argc, char* argv[])
 {
-    cout << "Command Line Tool Executed" << endl; // debug
-    cout << "Number of arguments: " << argc << endl; // debug
     int offset = 1; // offset to get to the argument value
     ifstream inFile;
     for (int i = 1; i < argc; ++i)
     {
-        // cout << "Argument " << i << ": " << *argv[i] << endl; // debug
-        // cout << "Full Argument String " << i << ": " << argv[i] << endl; // debug
-
         if (*argv[i] == '-') // if option flag given
         {
             char option = tolower(argv[i][1]); // force to lowercase
-            cout << "Option detected: " << option << endl; // debug
             switch (option)
             {
             case 'h':
-                cout << "Help option selected." << endl;
-                oss << string(argv[i + offset]) << endl;
-                cout << oss.str(); // debug
+                displayHelp();
+                clearOss();
                 break;
             case 'c':
-                cout << "Count bytes option selected." << endl;
-                oss << string(argv[i + offset]) << endl;
-                // assign to variable
+                // 1. open file at given path
                 inFile.open(argv[i + offset]);
+
+                // 2. check if it opened correctly
                 if (!inFile) {
-                    cerr << "Error opening file: " << argv[i + offset] << endl;
-                    return 1;
+                    // output error message from errno
+                    // Thank god this exists. 
+                    // I did NOT want to make my own error table with function calls for each one lol
+                    cerr << strerror(errno) << endl;
+
+                    // output error 
+                    cerr << "Invalid file path: " << argv[i + offset] << endl;
+
+                    cerr.clear(); // clear error state
+                    return 1; // exit with error code
+                    // TODO: Find some sort of test cases for this.
                 }
+
+                // 3. variable to hold byte count, I wanted this inline but... I guess you can't
+                // and I want to potentially do multiple operations later
+                int byteCount;
+
+                // 4. Call relevant function (here it's countBytes for returning int byte count)
+                byteCount = countBytes(inFile);
+
+                // 5. output result
+                cout << "Byte count: " << byteCount << endl;
+
+                // 6. close file
                 inFile.close();
-                cout << oss.str(); // debug
+
+                // return number of bytes as output for potential scripting use?
+                // return byteCount;
                 break;
             case 'l':
-                cout << "Count lines option selected." << endl;
-                oss << string(argv[i + offset]) << endl;
-                cout << oss.str(); // debug
+                // same steps as above, we are outputting and int.
+                inFile.open(argv[i + offset]);
+                if (!inFile) {
+                    cerr << strerror(errno) << endl;
+                    // output error 
+                    cerr << "Invalid file path: " << argv[i + offset] << endl;
+                    cerr.clear(); // clear error state
+                    return 1; // exit with error code
+                }
+                int lineCount;
+                lineCount = countLines(inFile);
+                cout << "Line count: " << lineCount << endl;
+                inFile.close();
                 break;
             case 'w':
+                // count words
                 cout << "Count words option selected." << endl;
-                oss << string(argv[i + offset]) << endl;
-                cout << oss.str(); // debug
+                // oss << string(argv[i + offset]) << endl;
+                // cout << oss.str(); // debug
                 break;
             default:
+                // unknown option
                 cout << "Unknown option: " << option << endl;
-                oss << string(argv[i + offset]) << endl;
-                cout << oss.str(); // debug
+                // oss << string(argv[i + offset]) << endl;
+                // cout << oss.str(); // debug
                 break;
             }
             i += offset; // skip next argument as it's a value
-            oss.str(""); // clear the stringstream for next use
-            oss.clear(); // clear any error flags
+            // oss.str(""); // clear the stringstream for next use
+            // oss.clear(); // clear any error flags
         }
         else
         {
             cout << "Non-option argument: " << argv[i] << endl;
-            oss << string(argv[i]) << endl;
+            // oss << string(argv[i]) << endl;
         }
 
     }
