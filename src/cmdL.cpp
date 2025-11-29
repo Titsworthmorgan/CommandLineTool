ef00c89a37138c51b194f5281f97fba1948b0639
Morgan Titsworth <titsworthmorgan@gmail.com>
2025-11-28 17:19:07 -0800

    Lots happening here

--- src/cmdL.cpp
+++ src/cmdL.cpp
@@ -1,17 +1,155 @@
-#include <iostream>
+#include "cmdL.hpp"
 #include <fstream>
 #include <iomanip>
-#include <numeric>
-#include <string>
-#include <sstream>
-#include <vector>
+#include <iostream>
 #include <map>
-#include "cmdL.hpp"
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 ostringstream oss;
 
-string displayHelp()
-{
+class Tokenizer {
+  private:
+    // Reference to input file stream
+    ifstream &inFile;
+
+  public:
+    // Enum to represent different types of tokens
+    enum class TokenType {
+        LEFT_BRACKET,
+        RIGHT_BRACKET,
+        COLON,
+        COMMA,
+        STRING,
+        END_OF_FILE,
+        UNKNOWN
+    };
+
+    // Struct to represent a token
+    struct Token {
+        TokenType type;
+        std::string value;
+    };
+
+    // Constructor - initializes with input file stream
+    Tokenizer(ifstream &file) : inFile(file) {}
+
+    // Function to get the next token from the input stream
+    Token getNextToken() {
+        char ch;
+        // Skip whitespace
+        while (inFile.get(ch)) {
+            if (!isspace(ch)) {
+                break;
+            }
+        }
+        // Check for end of file
+        if (inFile.eof()) {
+            // return empty string token
+            return {TokenType::END_OF_FILE, ""};
+        }
+        // Identify token type
+        switch (ch) {
+            case '{':
+                return {TokenType::LEFT_BRACKET, "{"};
+            case '}':
+                return {TokenType::RIGHT_BRACKET, "}"};
+            case ',':
+                return {TokenType::COMMA, ","};
+            case ':':
+                return {TokenType::COLON, ":"};
+            case '"': {
+                std::string strValue;
+                while (inFile.get(ch) && ch != '"') {
+                    strValue += ch;
+                }
+                return {TokenType::STRING, strValue};
+            }
+            default:
+                return {TokenType::UNKNOWN, std::string(1, ch)};
+        }
+    }
+};
+
+class Parser {
+  private:
+    // Tokenizer instance
+    Tokenizer tokenizer;
+
+  public:
+    // Enum to represent the type of JSON value
+    enum class valueType {
+        STRING,
+        UNKNOWN,
+    };
+
+    // Struct to represent a JSON value
+    struct JSONValue {
+        valueType type;
+        std::string stringValue;
+    };
+    // Constructor - initializes with input file stream
+    Parser(ifstream &file) : tokenizer(file) {}
+    // Function to parse JSON and return a map of key-value pairs
+    map<string, JSONValue> parse() {
+        // Map to hold key-value pairs
+        map<string, JSONValue> jsonMap;
+        // Get the first token
+        Tokenizer::Token token = tokenizer.getNextToken();
+        // Expecting a left bracket to start the JSON object
+        if (token.type != Tokenizer::TokenType::LEFT_BRACKET) {
+            throw runtime_error("Expected '{' at the beginning of JSON object");
+        }
+        // Loop to parse key-value pairs
+        while (true) {
+            // Get key
+            token = tokenizer.getNextToken();
+            // Check for end of object
+            if (token.type == Tokenizer::TokenType::RIGHT_BRACKET) {
+                break; // End of JSON object
+            }
+            // Expecting a string key
+            if (token.type != Tokenizer::TokenType::STRING) {
+                throw runtime_error("Expected string key in JSON object");
+            }
+            // Store key
+            string key = token.value;
+            // Expecting colon
+            token = tokenizer.getNextToken();
+            if (token.type != Tokenizer::TokenType::COLON) {
+                throw runtime_error("Expected ':' after key in JSON object");
+            }
+            // Get value
+            token = tokenizer.getNextToken();
+            if (token.type != Tokenizer::TokenType::STRING) {
+                throw runtime_error("Expected string value in JSON object");
+            }
+            // Store value
+            JSONValue value;
+            // For simplicity, we only handle string values for now
+            value.type = valueType::STRING;
+            // Assign string value
+            value.stringValue = token.value;
+            // Insert key-value pair into map
+            jsonMap[key] = value;
+            // Get next token (either comma or end of object)
+            token = tokenizer.getNextToken();
+            // Check for comma or end of object
+            if (token.type == Tokenizer::TokenType::COMMA) {
+                continue; // Next key-value pair
+            } else if (token.type == Tokenizer::TokenType::RIGHT_BRACKET) {
+                break; // End of JSON object
+            } else {
+                throw runtime_error("Expected ',' or '}' in JSON object");
+            }
+        }
+        return jsonMap;
+    }
+};
+
+string displayHelp() {
     oss << "Command Line Tool Help Information\n"
         << "Usage: bash test.sh [options] [filename]\n"
         << "Options:\n"
@@ -23,20 +161,18 @@ string displayHelp()
     return oss.str();
 }
 
-int countBytes(ifstream &inFile)
-{
+int countBytes(ifstream &inFile) {
     // clear any eof flags
     inFile.clear();
 
     // skip to end
     inFile.seekg(0, ios::end);
-    
+
     // return position as byte count
     return inFile.tellg();
 }
 
-int countLines(ifstream &inFile)
-{
+int countLines(ifstream &inFile) {
     // line variable
     string line;
 
@@ -50,8 +186,7 @@ int countLines(ifstream &inFile)
     inFile.seekg(0, ios::beg);
 
     // while line exists, increment count
-    while (getline(inFile, line))
-    {
+    while (getline(inFile, line)) {
         lineCount++;
     }
 
@@ -59,8 +194,7 @@ int countLines(ifstream &inFile)
     return lineCount;
 }
 
-int countWords(ifstream &inFile)
-{
+int countWords(ifstream &inFile) {
     // word variable to hold each word
     // this will be delimited by whitespace by default.
     // really we're just taking advantage of the stream extraction operator here and its default behavior.
@@ -76,8 +210,7 @@ int countWords(ifstream &inFile)
     inFile.seekg(0, ios::beg);
 
     // while word exists, increment count
-    while (inFile >> word)
-    {
+    while (inFile >> word) {
         wordCount++;
     }
 
@@ -85,65 +218,30 @@ int countWords(ifstream &inFile)
     return wordCount;
 }
 
-enum class TokenType {
-    LEFT_BRACE, RIGHT_BRACE,
-    LEFT_BRACKET, RIGHT_BRACKET,
-    COLON, COMMA,
-    STRING, NUMBER,
-    END_OF_FILE,
-    UNKNOWN
-};
+void parseAndWriteJsonFiles(ifstream &file1, ofstream &file2) {
+    try {
+        // Create a parser instance with the input file stream
+        Parser parser(file1);
+        // Parse the JSON and get the key-value map
+        map<string, Parser::JSONValue> jsonMap = parser.parse();
 
-struct Token {
-    TokenType type;
-    std::string value;
-};
-
-enum class valueType {
-    STRING,
-    NUMBER,
-    OBJECT,
-    ARRAY,
-    NONE
-};
-
-struct JSONValue {
-    valueType type;
-    std::string stringValue;
-    std::vector<JSONValue> arrayValue;
-    std::map<std::string, JSONValue> objectValue;
-};
-
-class Tokenizer
-{
-    private: 
-    public:
-};
-
-class Parser
-{
-    private:
-    public:
-};
-
-string parseJsonFiles(ifstream &file1, ofstream &file2)
-{
-    // Placeholder implementation
-    oss << "JSON parsing not yet implemented.\n";
-    cout << oss.str();
-    return oss.str();
-    // maybe use classes for tokenizer and parser?
-    // or could I use a series of functions?
-    // Note for later
-    // What might work best is to have the tokenizer class produce a vector of tokens
-    // Then have the parser class take that vector and produce a JSONValue object
-    // Finally have a function that takes a JSONValue object and serializes it to the output file
-    // This would separate concerns nicely
-    // This should work.
+        // Write parsed key-value pairs to output file
+        for (const auto &pair : jsonMap) {
+            if (pair.second.type == Parser::valueType::STRING) {
+                file2 << pair.first << ": " << pair.second.stringValue << endl;
+            } else {
+                file2 << pair.first << ": [Unsupported Type]" << endl;
+            }
+        }
+    } catch (const runtime_error &e) {
+        cerr << "Error parsing JSON: " << e.what() << endl;
+        return;
+    }
+    clearOss();
+    return;
 }
 
-void clearOss()
-{
+void clearOss() {
     oss.str(""); // clear the stringstream
     oss.clear(); // clear any error flags
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include "cmdL.hpp"
-#include <iostream>
+#include <cstring>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <limits>
+#include <map>
 #include <numeric>
 #include <sstream>
 #include <string>
-#include <iomanip>
-#include <cstring>
+#include <vector>
 using namespace std;
 
 // yes I want these lowercase
-enum CommandLineOptions
-{
+enum CommandLineOptions {
     h, // help
     c, // count bytes
     l, // count lines
@@ -19,137 +20,126 @@ enum CommandLineOptions
     j, // json parsing between two files
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     ifstream inFile;
     // post increment, we are potentially skipping an element by using an offset.
-    for (int i = 1; i < argc; i++)
-    {
+    for (int i = 1; i < argc; i++) {
         if (*argv[i] == '-') // Capture option flags
         {
             char option = tolower(argv[i][1]); // force to lowercase
-            switch (option)
-            {
-            case 'h':
-            {
-                displayHelp();
-                clearOss();
-                break;
-            }
-            case 'c':
-            {
-                // 1. open file at given path
-                inFile.open(argv[i + 1]);
-
-                // 2. check if it opened correctly
-                if (!inFile)
-                {
-                    // output error message from errno
-                    // Thank god this exists.
-                    // I did NOT want to make my own error table with function calls for each one lol
-                    cerr << strerror(errno) << endl;
-
-                    // output error
-                    cerr << "Invalid file path: " << argv[i + 1] << endl;
-
-                    cerr.clear(); // clear error state
-                    return 1;     // exit with error code
-                    // TODO: Find some sort of test cases for this.
+            switch (option) {
+                case 'h': {
+                    displayHelp();
+                    clearOss();
+                    break;
                 }
+                case 'c': {
+                    // 1. open file at given path
+                    inFile.open(argv[i + 1]);
 
-                // 3. variable to hold byte count, I wanted this inline but... I guess you can't
-                // and I want to potentially do multiple operations later
-                int byteCount;
+                    // 2. check if it opened correctly
+                    if (!inFile) {
+                        // output error message from errno
+                        // Thank god this exists.
+                        // I did NOT want to make my own error table with function calls for each one lol
+                        cerr << strerror(errno) << endl;
 
-                // 4. Call relevant function (here it's countBytes for returning int byte count)
-                byteCount = countBytes(inFile);
+                        // output error
+                        cerr << "Invalid file path: " << argv[i + 1] << endl;
 
-                // 5. output result
-                cout << "Byte count: " << byteCount << endl;
+                        cerr.clear(); // clear error state
+                        return 1;     // exit with error code
+                        // TODO: Find some sort of test cases for this.
+                    }
 
-                // 6. close file
-                inFile.close();
+                    // 3. variable to hold byte count, I wanted this inline but... I guess you can't
+                    // and I want to potentially do multiple operations later
+                    int byteCount;
 
-                // return number of bytes as output for potential scripting use?
-                // return byteCount;
-                break;
-            }
-            case 'l':
-            {
-                // same steps as above, we are outputting and int.
-                inFile.open(argv[i + 1]);
-                if (!inFile)
-                {
-                    cerr << strerror(errno) << endl;
-                    // output error
-                    cerr << "Invalid file path: " << argv[i + 1] << endl;
-                    cerr.clear(); // clear error state
-                    return 1;     // exit with error code
+                    // 4. Call relevant function (here it's countBytes for returning int byte count)
+                    byteCount = countBytes(inFile);
+
+                    // 5. output result
+                    cout << "Byte count: " << byteCount << endl;
+
+                    // 6. close file
+                    inFile.close();
+
+                    // return number of bytes as output for potential scripting use?
+                    // return byteCount;
+                    break;
                 }
-                int lineCount;
-                lineCount = countLines(inFile);
-                cout << "Line count: " << lineCount << endl;
-                inFile.close();
-                break;
-            }
-            case 'w':
-            {
-                // same steps
-                inFile.open(argv[i + 1]);
-                if (!inFile)
-                {
-                    cerr << strerror(errno) << endl;
-                    // output error
-                    cerr << "Invalid file path: " << argv[i + 1] << endl;
-                    cerr.clear(); // clear error state
-                    return 1;     // exit with error code
+                case 'l': {
+                    // same steps as above, we are outputting and int.
+                    inFile.open(argv[i + 1]);
+                    if (!inFile) {
+                        cerr << strerror(errno) << endl;
+                        // output error
+                        cerr << "Invalid file path: " << argv[i + 1] << endl;
+                        cerr.clear(); // clear error state
+                        return 1;     // exit with error code
+                    }
+                    int lineCount;
+                    lineCount = countLines(inFile);
+                    cout << "Line count: " << lineCount << endl;
+                    inFile.close();
+                    break;
                 }
-                int wordCount;
-                wordCount = countWords(inFile);
-                cout << "Word count: " << wordCount << endl;
-                inFile.close();
-                break;
-            }
-            // json parsing
-            case 'j':
-            {
-                // open both files this time
-                ifstream file1;
-                ofstream file2;
-                file1.open(argv[i + 1]);
-                file2.open(argv[i + 2]);
-                if (!file1)
-                {
-                    cerr << strerror(errno) << endl;
-                    cerr << "Invalid file path: " << argv[i + 1] << endl;
-                    cerr.clear();
-                    return 1;
+                case 'w': {
+                    // same steps
+                    inFile.open(argv[i + 1]);
+                    if (!inFile) {
+                        cerr << strerror(errno) << endl;
+                        // output error
+                        cerr << "Invalid file path: " << argv[i + 1] << endl;
+                        cerr.clear(); // clear error state
+                        return 1;     // exit with error code
+                    }
+                    int wordCount;
+                    wordCount = countWords(inFile);
+                    cout << "Word count: " << wordCount << endl;
+                    inFile.close();
+                    break;
                 }
-                if (!file2)
-                {
-                    cerr << strerror(errno) << endl;
-                    cerr << "Invalid file path: " << argv[i + 2] << endl;
-                    cerr.clear();
-                    return 1;
+                // json parsing
+                case 'j': {
+                    // open both files this time
+                    ifstream file1;
+                    ofstream file2;
+                    file1.open(argv[i + 1]);
+                    file2.open(argv[i + 2]);
+                    if (!file1) {
+                        cerr << strerror(errno) << endl;
+                        cerr << "Invalid file path: " << argv[i + 1] << endl;
+                        cerr.clear();
+                        return 1;
+                    }
+                    if (!file2) {
+                        cerr << strerror(errno) << endl;
+                        cerr << "Invalid file path: " << argv[i + 2] << endl;
+                        cerr.clear();
+                        return 1;
+                    }
+                    try {
+                        parseAndWriteJsonFiles(file1, file2);
+                    } catch (const runtime_error &e) {
+                        file1.close();
+                        file2.close();
+                        return 1;
+                    }
+                    // close files
+                    file1.close();
+                    file2.close();
+                    i += 1; // increment an additional time as we used two file paths
+                    break;
                 }
-                // call json parsing function with both refs
-                parseJsonFiles(file1, file2);
-                // close files
-                file1.close();
-                file2.close();
-                i += 1; // increment an additional time as we used two file paths
-                break;
-            }
-            default:
-            {
-                cout << "Unknown option: " << option << endl;
-                break;
-            }
+                default: {
+                    cout << "Unknown option: " << option << endl;
+                    break;
+                }
             } // end switch
             i += 1; // skip next argument as it's a value
-        }
-        else
-        {
+        } else {
             cout << "Non-option argument: " << argv[i] << endl;
             // oss << string(argv[i]) << endl;
         }
